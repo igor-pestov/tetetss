9bd4d90a591dff94aab23f293e74aee2a48de2a8
igor-pestov <igor.pestov.mern@gmail.com>
2021-05-07 19:04:04 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Announcement from './pages/Announcement';
 
 function App() {
-
+  localStorage.getItem('lang')
+    ? localStorage.getItem('lang')
+    : localStorage.setItem('lang', 'rus');
   return (
     <div className='App'>
       <Announcement />
